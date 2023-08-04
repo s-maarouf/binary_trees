972aa90895e4d286a8693515a313856922ccd5a4
smaarouf <maarouf.salahedd@gmail.com>
2023-08-04 02:06:24 +0100

    Completed task 19

--- binary_trees.h
+++ binary_trees.h
@@ -53,5 +53,7 @@ size_t depth(const binary_tree_t *tree);
 unsigned char is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 
 #endif
