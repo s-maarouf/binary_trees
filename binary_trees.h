03ee04172541bd282e4bc5f4f0a15b701d8a9801
Salah Eddine Maarouf <75632264+PRIZZMO@users.noreply.github.com>
2023-08-04 20:20:02 +0100

    Fixed betty style code

--- binary_trees.h
+++ binary_trees.h
@@ -15,10 +15,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -60,13 +60,13 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int is_full_recursive(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_depth, size_t level);
+						 size_t leaf_depth, size_t level);
 const binary_tree_t *get_leaf(const binary_tree_t *tree);
 size_t depth(const binary_tree_t *tree);
 unsigned char is_leaf(const binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second);
+									 const binary_tree_t *second);
 
 #endif
