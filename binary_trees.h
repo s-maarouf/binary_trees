f6341ff4e6f44fd6aeb2bf7e036988e2c2749a11
smaarouf <maarouf.salahedd@gmail.com>
2023-08-04 02:17:19 +0100

    Completed task 21

--- binary_trees.h
+++ binary_trees.h
@@ -72,5 +72,11 @@ void pint_push(binary_tree_t *node, levelorder_queue_t *head,
 		levelorder_queue_t **tail, void (*func)(int));
 void free_queue(levelorder_queue_t *head);
 void pop(levelorder_queue_t **head);
+int binary_tree_is_complete(const binary_tree_t *tree);
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail);
+void pop(levelorder_queue_t **head);
 
 #endif
