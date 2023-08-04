b0c8ff372a20f316ad6a18d37c51a4ffc21f4b69
smaarouf <maarouf.salahedd@gmail.com>
2023-08-04 01:18:34 +0100

    Fixed typo error

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,9 @@
-#define binary_trees_h
-#ifdef binary_trees_h
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -16,10 +15,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
