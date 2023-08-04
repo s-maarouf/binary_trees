faf34672785a8837641f1d31a30727862591b9e7
smaarouf <maarouf.salahedd@gmail.com>
2023-08-04 02:02:37 +0100

    Completed task 16

--- binary_trees.h
+++ binary_trees.h
@@ -45,6 +45,11 @@ int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int is_full_recursive(const binary_tree_t *tree);
-
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int is_perfect_recursive(const binary_tree_t *tree,
+		size_t leaf_depth, size_t level);
+const binary_tree_t *get_leaf(const binary_tree_t *tree);
+size_t depth(const binary_tree_t *tree);
+unsigned char is_leaf(const binary_tree_t *node);
 
 #endif
